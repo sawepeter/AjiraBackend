8f3947b5ca2d8966287734247046939bbf0563f1
sawepeter <sawepeter6@gmail.com>
2020-02-12 18:43:11 +0300

    intial commit

--- src/main/java/com/devsawe/demo/controllers/TodoController.java
+++ src/main/java/com/devsawe/demo/controllers/TodoController.java
@@ -6,14 +6,15 @@ import com.devsawe.demo.entities.TodoModel;
 import com.devsawe.demo.repositories.TodoRepository;
 import com.devsawe.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class TodoController {
@@ -23,13 +24,6 @@ public class TodoController {
 
     @Autowired
     private UserRepository userRepository;
-
-    //doesnot work but works with the my-todos path.
-   /* @GetMapping("/users/{userId}/todolist")
-    public Page<TodoModel> getAllTodoListByUserId(@PathVariable(value = "UserId") Long userId,
-                                                  Pageable pageable) {
-        return todoRepository.findByUserId(userId, pageable);
-    }*/
 
     //works creates todo list for the currently logged in user.
     @PostMapping("/users/todos")
@@ -41,34 +35,50 @@ public class TodoController {
         return todoRepository.save(todoModel);
     }
 
-    @PutMapping("users/{userId}/todolist/{todoId}")
-    public TodoModel updateTodoList(@PathVariable(value = "userId") Long userId,
-                                    @PathVariable(value = "todoId") Long todoId,
-                                    @Valid @RequestBody TodoModel todoRequest) {
-        if (!userRepository.existsById(userId)) {
-            throw new ResourceNotFoundException("userId" + userId + "not found");
-        }
+    @PutMapping("my-todos/{id}")
+   public ResponseEntity<?> update(@PathVariable long id, @RequestBody TodoModel todoModel){
+        Map<String, String> resp = new HashMap<>();
+     CustomUserDetails customUserDetails = (CustomUserDetails)
+        SecurityContextHolder.getContext()
+            .getAuthentication().getPrincipal();
+     TodoModel todoModel1 = todoRepository.findById(id).orElse(null);
 
-        return todoRepository.findById(todoId).map(todoModel -> {
-            todoModel.setTodotitle(todoRequest.getTodotitle());
-            return todoRepository.save(todoModel);
-        }).orElseThrow(() -> new ResourceNotFoundException("todoId" + todoId + "not found"));
+     if (todoModel1 == null){
+         resp.put("state", "danger");
+         resp.put("msg", "id not found");
+         return ResponseEntity.ok(resp);
+     }
+     /*todoModel1.setId(todoModel.getId());*/
+     todoModel1.setTodotitle(todoModel.getTodotitle());
+     todoModel1.setTodotime(todoModel.getTodotime());
+     todoModel1.setUserId(customUserDetails.getId());
+     todoRepository.save(todoModel1);
+     resp.put("state", "success");
+     resp.put("msg", "Todo updated successfully");
+     return  ResponseEntity.ok(resp);
     }
 
-    //delete todo list when user is logged in
-    @DeleteMapping("/my-todos/delete/{id}")
-    public ResponseEntity<?> deleteTodoList(@PathVariable(value = "todoId") Long todoId) {
+    @PutMapping("my-todos/completed/{id}")
+    public ResponseEntity<?> completed(@PathVariable long id){
+        Map<String, String> resp = new HashMap<>();
         CustomUserDetails customUserDetails = (CustomUserDetails)
                 SecurityContextHolder.getContext()
-                        .getAuthentication().getPrincipal();
-        TodoModel todoModel = this.todoRepository.findById(todoId).orElse(null);
-        if (todoModel != null && todoModel.getUserId() == customUserDetails.getId()) {
-            todoRepository.delete(todoModel);
+                .getAuthentication().getPrincipal();
+        TodoModel todoModel1 = todoRepository.findById(id).orElse(null);
+
+        if (todoModel1 == null){
+            resp.put("state", "danger");
+            resp.put("msg","The user does not a todo with that id");
+            return ResponseEntity.ok(resp);
         }
 
-        return ResponseEntity.noContent().build();
+        todoModel1.setStatus("Completed");
+        todoModel1.setUserId(customUserDetails.getId());
+        todoRepository.save(todoModel1);
+        resp.put("state", "success");
+        resp.put("msg", "state updated successfully");
+        return ResponseEntity.ok(resp);
     }
-
     //get the todo list of the current user(logged in)
     @GetMapping("/my-todos")
     public ResponseEntity<List<TodoModel>> currentUserToDos() {
@@ -78,4 +88,49 @@ public class TodoController {
         List<TodoModel> todoModels = todoRepository.findByUserId(customUserDetails.getId());
         return ResponseEntity.ok(todoModels);
     }
+
+    //get the todotitle by a specific date
+    @GetMapping("/today-todos")
+    public ResponseEntity<List<TodoModel>> todayToDos(@RequestParam(value = "date") String date ){
+        CustomUserDetails customUserDetails = (CustomUserDetails)
+                SecurityContextHolder.getContext()
+                    .getAuthentication().getPrincipal();
+        List<TodoModel> todoModels = todoRepository.findByCreatedAt(customUserDetails.getId(),date);
+        return ResponseEntity.ok(todoModels);
+    }
+
+    @DeleteMapping("/my-todos/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") long id){
+        CustomUserDetails customUserDetails = (CustomUserDetails)
+            SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+        TodoModel todoModel = todoRepository.findById(id).orElse(null);
+
+        if (todoModel == null){
+            return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+        }
+        todoRepository.delete(todoModel);
+        return new ResponseEntity<Void>(HttpStatus.OK);
+
+    }
+
+ /*   @GetMapping("/mytodos-today")
+    public ResponseEntity<List<Map<String, String>>> Todostoday(@RequestParam(value = "date") String date){
+        CustomUserDetails customUserDetails = (CustomUserDetails)
+                SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+        List<Map<String, String>> todotitle;
+         todotitle = todoRepository.findByCreatedAt(customUserDetails.getId(),date);
+        return  ResponseEntity.ok(todotitle);
+    }*/
+
+    /*@GetMapping("/mytodos-week")
+    public ResponseEntity<List<Map<String, String>>> TodosWeek(){
+        CustomUserDetails customUserDetails = (CustomUserDetails)
+                SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+        List<Map<String, String>> todotitle;
+        todotitle = todoRepository.findByWeek(customUserDetails.getId());
+        return ResponseEntity.ok(todotitle);
+    }*/
 }
--- src/main/java/com/devsawe/demo/repositories/TodoRepository.java
+++ src/main/java/com/devsawe/demo/repositories/TodoRepository.java
@@ -4,9 +4,13 @@ import com.devsawe.demo.entities.TodoModel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +18,16 @@ public interface TodoRepository extends JpaRepository<TodoModel, Long> {
     Page<TodoModel> findByUserId(Long userId, Pageable pageable);
     List<TodoModel> findByUserId(Long userId);
     Optional<TodoModel> findByIdAndUserId(Long id, Long UserId);
+
+    @Query(value = "SELECT d.* FROM todo d WHERE DATE_FORMAT(d.created_at, '%Y-%m-%d') = :date AND d.user_id = :id", nativeQuery = true)
+    List<TodoModel> findByCreatedAt(@Param("id") Long id, @Param("date") String date);
+
+   /* //query for today
+    @Query(value = "SELECT todotitle,todotime FROM todo d WHERE DATE_FORMAT(d.created_at, '%Y-%m-%d') = :date AND d.user_id = :id", nativeQuery = true)
+    List<Map<String, String>> findByCreatedAt(@Param("id") Long id, @Param("date") String date);*/
+
+
+   /* @Query(value = "SELECT todotitle,todotime FROM todo d WHERE DATE_FORMAT(d.created_at, '%Y-%m-%d') => DATE_SUB(CURDATE(), INTERVAL 7 DAY) AND d.user_id = :id", nativeQuery = true)
+    List<Map<String, String>> findByWeek(@Param("id") Long id);*/
+
 }
